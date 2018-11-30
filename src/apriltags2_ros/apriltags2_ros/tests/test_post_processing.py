1cd57f7a899f7ec8b4c32beeb2a60e6847976ca1
Selcuk Ercan <ercans@ethz.ch>
2018-11-30 15:59:03 +0100

    the skeleton for the unit tests is established

--- src/apriltags2_ros/apriltags2_ros/tests/test_post_processing.py
+++ src/apriltags2_ros/apriltags2_ros/tests/test_post_processing.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
-import unittest
+import unittest, rosunit
 import numpy as np
 
 class TestPostProcessor(unittest.TestCase):
     def test_inverse_homogenous_transform(self):
-        self.assertEqual(1, 0)
+        self.assertEqual(0, 0)
 
 if __name__ == '__main__':
-    unittest.main()
+    rosunit.unitrun('apriltags2_ros', 'test_post_processing', TestPostProcessor)
