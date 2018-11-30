41160c089b74d308ca6b17e88d86dc43d7295c3e
Selcuk Ercan <ercans@ethz.ch>
2018-11-30 21:53:09 +0100

    added tests [2]

--- src/apriltags2_ros/apriltags2_ros/tests/test_rotation_utils.py
+++ src/apriltags2_ros/apriltags2_ros/tests/test_rotation_utils.py
@@ -37,19 +37,20 @@ class RotationUtils(unittest.TestCase):
         T_inv = inverse_homogeneous_transform(T)
         np.testing.assert_almost_equal(T_inv, T_inv_expected)
 
+    def test_inverse_homogenous_transform_translation_rotation(self):
+        # Spesifically tests matrix inversion used in the function.
+        a_T_b = tr.identity_matrix()
+        a_T_b[0:3,0:3] = np.array([[0,-1,0],[1,0,0],[0,0,1]]) # rotation around z axis by 90 degrees. frame B is rotated with respect to az
+        a_T_b[0:3,3] = np.array([1,1,0]) # translation by 1 unit in positive x. translation from from frame A to frame B expressed in frame A.
+
+        b_T_a_exp = tr.identity_matrix()
+        b_T_a_exp[0:3,3] = np.matmul(np.array([[0,1,0],[-1,0,0],[0,0,1]]), -np.array([1,1,0]))
+        b_T_a_exp[0:3,0:3] = np.array([[0,1,0],[-1,0,0],[0,0,1]]) # R_inv * -t
+
+        T_inv = inverse_homogeneous_transform(a_T_b)
+        np.testing.assert_almost_equal(T_inv, b_T_a_exp)
+
     """
-    def test_inverse_homogenous_transform_translation_combined(self):
-        T = tr.identity_matrix()
-        T = np.array([[0,1,0],[-1,0,0],[0,0,0]]) # rotation around z axis by 90 degrees
-        T[0:3,3] = np.array([1,0,0]) # shifted by 1 unit in positive x
-
-        T_inv_expected = tr.identity_matrix()
-        T_inv_expected[0:3,3] = np.array([-1,-1,-1])
-
-        T_inv = inverse_homogeneous_transform(T)
-        np.testing.assert_almost_equal(T_inv, T_inv_expected)
-
-
     def test_inverse_homogenous_transform2(self):
         self.assertEqual(1, 0)
     """
