03554c4901b5bd840b4cb140b5dd1fcf8b9d73ed
selcukercan <ercans@ethz.com>
2018-12-02 01:08:47 +0100

    added/formatted tests + robot2tag

--- src/apriltags2_ros/apriltags2_ros/include/apriltags2_ros_post_process/rotation_utils.py
+++ src/apriltags2_ros/apriltags2_ros/include/apriltags2_ros_post_process/rotation_utils.py
@@ -1,15 +1,23 @@
 import tf.transformations as tr
 import numpy as np
 import rospy
+import math
 
-TILT  = 19
+TILT_ANGLE = 19 * math.pi / 180.0
+
 
 def col(v):
+    '''
+    Convert a numpy singleton into a column array
+    '''
     return np.expand_dims(v, axis=1)
 def row(v):
+    '''
+    Convert a numpy singleton into a row array
+    '''
     return np.expand_dims(v, axis=0)
 
-def QuaternionToEuler( x , y , z , w ):
+def quaternion_to_euler( x , y , z , w ):
     q0 = w;
     q1 = x;
     q2 = y;
@@ -28,14 +36,17 @@ def QuaternionToEuler( x , y , z , w ):
 
     return (rx,ry,rz)
 
-def TagDetection2HomegenousTransformation(q,t):
+def camztiltedTtag(q,t):
+    '''
+    Expresesses the apriltag2_ros output (message includes a quaterion and a translation vector)
+    in homogeneous transform matrix.
+    '''
     # quaterion convention (x,y,z,w)
-    """
-    q = [0.5, 0, 0, 1]
-    t = [1,1,1]
+
     """
     q = [q.x, q.y, q.z, q.w]
     t = [t.x, t.y, t.z]
+    """
 
     T_q = tr.quaternion_matrix(q)
     T = T_q.copy()
@@ -44,11 +55,14 @@ def TagDetection2HomegenousTransformation(q,t):
     return T
 
 def inverse_homogeneous_transform(T):
+    '''
+    Inverts a homogeneous transformation matrix using the formula 2.92 of [1].
+    '''
     t = T[0:3,3]
     R = T[0:3,0:3]
 
     T_inv = tr.identity_matrix()
-    R_inv = tr.inverse_matrix(R)
+    R_inv = np.transpose(R)
 
     t_inv = np.matmul(R_inv,-t)
 
@@ -57,39 +71,133 @@ def inverse_homogeneous_transform(T):
 
     return T_inv
 
-def Camz2CamzTilt():
-    """
-        Returns Homogeneous Transformation that corresponds to Coordinate Transformation (active rotation) from z camera to x camera frame.
-        Z camera frame:
-            Seeing through camera:
-                X -> right
-                Y -> down
-                Z -> forward
-        X camera frame:
-            Seeing through camera:
-                X -> forward
-                Y -> left
-                Z -> up
+def camztiltedTcamz(TILT_ANGLE):
+    '''
+    Returns homogeneous transformation that expresses a camz_p in camz frame.
+    '''
+    d = 0.05
 
-        endFrame_Tp_initFrame:
-        Represents passive Transformation, i.e represented vector remains the same wrt to a fixed world frame.
-        The matrix describes how the basis vectors of are related to each other, spesifically it translates
-        a vector expressed in initFrame into a vector expressed in endFrame.
+    camztilted_T_camz = tr.rotation_matrix(TILT_ANGLE, [1,0,0])
+    t = np.array([0, d*(1 - math.cos(TILT_ANGLE)), d*math.sin(TILT_ANGLE)])
+    camztilted_T_camz[0:3,3] = t
 
-        endFrame_R_initFrame:
-        represents relative orientation of endFrame wrt. initFrame, and is obtained by a series of consecutive relative rotations with rotations post-multiplied:
-        R = R1 * R2 * ...
+    return camztilted_T_camz
 
-        endFrame_t_initFrame:
-        represent the position vector from the origin of initFrame to the origin of endFrame expressend in initFrame
-    """
-    # Initially compensate for the tilt of the camera frame.
-    camztilted_T_camz = tr.rotation_matrix(TILT, [1,0,0])
+def camzTcamztilted():
+    '''
+    Returns homogeneous transformation that expresses camztilted_p in camz frame.
+    '''
+    camztilted_T_camz = camztiltedTcamz(TILT_ANGLE)
     camz_T_camztilted = inverse_homogeneous_transform(camztilted_T_camz)
 
-    print camz_R_camztilted
-    #camz_T_camztilted = tr.identity_matrix()
-    #camz_T_camztilted[0:3,0:3] = camz_R_camztilted
-
-    #print camz_T_camztilted
     return camz_T_camztilted
+
+def camzTcamx():
+    '''
+    Returns homogeneous transformation that expresses camx_p in camz frame.
+    '''
+    T1 = tr.rotation_matrix(math.pi / 2, [1,0,0])
+    T2 = tr.rotation_matrix(math.pi / 2, [0,0,1])
+    camz_T_camx = np.matmul(T1, T2)
+    return camz_T_camx
+
+def camxTcamz():
+    '''
+    Returns homogeneous transformation that expresses camz_p in camx frame.
+    '''
+    camz_T_camx = camzTcamx()
+    camx_T_camz = inverse_homogeneous_transform(camz_T_camx)
+    return camx_T_camz
+
+def vehTcamx(tOvehOcamx):
+    veh_T_camx = tr.translation_matrix(tOvehOcamx)
+    return veh_T_camx
+
+def camxTveh(tOvehOcamx):
+    veh_T_camx = vehTcamx(tOvehOcamx)
+    camx_T_veh = inverse_homogeneous_transform(veh_T_camx)
+    return camx_T_veh
+
+def get_robot_pose(q_at,t_at):
+    tOvehOcamx = np.array([0.10,0.0,0.05])
+
+    camztilted_T_tag = camztiltedTtag(q_at,t_at)
+    camztilted_T_camz = camztiltedTcamz(TILT_ANGLE)
+    camx_T_camz = camxTcamz()
+    veh_T_camx = vehTcamx(tOvehOcamx)
+
+    D1 = np.matmul(veh_T_camx,camx_T_camz)
+    # TODO: camztilted_T_camz mı lazım emin ol
+    D2 = np.matmul(D1, camztilted_T_camz)
+    veh_T_tag = np.matmul(D2,camztilted_T_tag)
+
+    veh_R_tag = veh_T_tag[0:3, 0:3]
+    veh_t_tag = veh_T_tag[0:3, 3]
+
+    """
+    
+    T1 = camztilted_T_tag * camz_T_camztilted
+    T2 = T1 * camx_T_camz
+    veh_T_tag =  T2 * veh_T_camx
+
+    """
+
+    """
+    camztilted_T_tag = camztiltedTtag(q_at, t_at)
+    camztilted_T_camz = camztiltedTcamz(TILT_ANGLE)
+    camz_T_camx = camzTcamx()
+    camx_T_veh = camxTveh(tOvehOcamx)
+
+    T1 = camztilted_T_tag * camztilted_T_camz
+    T2 = T1 * camz_T_camx
+    tag_T_veh = T2 * camx_T_veh
+    veh_R_tag = tag_T_veh[0:3, 0:3]
+    veh_t_tag = tag_T_veh[0:3, 3]
+    """
+
+    return veh_R_tag, veh_t_tag
+
+'''
+[NOMENCLATURE]
+
+Z camera frame:
+    seeing what camera sees:
+        X -> right
+        Y -> down
+        Z -> forward
+X camera frame:
+    seeing what camera sees:
+        X -> forward
+        Y -> left
+        Z -> up
+
+endFrame_Tp_initFrame:
+Represents passive Transformation, i.e represented vector remains the same wrt to a fixed world frame.
+The matrix describes how the basis vectors of are related to each other, spesifically it translates
+a vector expressed in initFrame into a vector expressed in endFrame.
+
+endFrame_R_initFrame:
+represents relative orientation of endFrame wrt. initFrame, and is obtained by a series of consecutive relative rotations with rotations post-multiplied:
+R = R1 * R2 * ...
+
+endFrame_t_initFrame:
+represent the position vector from the origin of initFrame to the origin of endFrame expressend in initFrame
+
+FRAME_p:
+position vector p expressed in FRAME frame, i.e  WORLD_p means p is in WORLD coordinate frame.
+'''
+
+'''
+Resources:
+
+[1] "Robot Dynamics and Control", Second Edition, Mark W. Spong, Seth Hutchinson, and M. Vidyasagar.
+
+[2] ROS TF library for python: https://github.com/ros/geometry/blob/hydro-devel/tf/src/tf/transformations.py
+'''
+
+if __name__ == '__main__':
+    q = [0.9585, 0.0263, -0.2431, -0.1467]
+    t = [0.016, -0.041, 0.295]
+
+    veh_R_tag, veh_t_tag = get_robot_pose(q, t)
+    print "Selcuk"
--- src/apriltags2_ros/apriltags2_ros/tests/test_rotation_utils.py
+++ src/apriltags2_ros/apriltags2_ros/tests/test_rotation_utils.py
@@ -2,13 +2,14 @@
 import unittest, rosunit, rospy
 import numpy as np
 import tf.transformations as tr
-from apriltags2_ros_post_process.rotation_utils import col, row, inverse_homogeneous_transform
+from apriltags2_ros_post_process.rotation_utils import *
 
 class RotationUtils(unittest.TestCase):
 
     def test_col(self):
         '''
-        Convert a numpy singleton into a column array
+        tests whether a numpy singleton is converted into a column array correctly,
+        (technically a numpy matrix of shape (n,1)).
         '''
         v = np.array([1,2,3])
         v_col = col(v)
@@ -16,18 +17,25 @@ class RotationUtils(unittest.TestCase):
 
     def test_row(self):
         '''
-        Convert a numpy singleton into a row array
+        tests whether a numpy singleton is converted into a row array correctly,
+        (technically a numpy matrix of shape (1,n)).
         '''
         v = np.array([1,2,3])
         v_row = row(v)
         self.assertEqual(v_row.shape, (1,3))
 
     def test_inverse_homogenous_transform_trivial(self):
+        '''
+        tests whether inverse_homogeneous_transform yields  T = inv(T) when T is the identity matrix.
+        '''
         T = tr.identity_matrix()
         T_inv = inverse_homogeneous_transform(T)
         np.testing.assert_almost_equal(T,T_inv)
 
     def test_inverse_homogenous_transform_non_zero_translation(self):
+        '''
+        tests whether inverse_homogeneous_transform performs the inversion correctly for T with non-zero translation vector.
+        '''
         T = tr.identity_matrix()
         T[0:3,3] = np.array([1,1,1])
 
@@ -38,6 +46,10 @@ class RotationUtils(unittest.TestCase):
         np.testing.assert_almost_equal(T_inv, T_inv_expected)
 
     def test_inverse_homogenous_transform_translation_rotation(self):
+        '''
+        tests whether inverse_homogeneous_transform performs the inversion correctly for
+        T with non-zero translation vector and a non-identity rotation matrix.
+        '''
         # Spesifically tests matrix inversion used in the function.
         a_T_b = tr.identity_matrix()
         a_T_b[0:3,0:3] = np.array([[0,-1,0],[1,0,0],[0,0,1]]) # rotation around z axis by 90 degrees. frame B is rotated with respect to az
@@ -50,9 +62,24 @@ class RotationUtils(unittest.TestCase):
         T_inv = inverse_homogeneous_transform(a_T_b)
         np.testing.assert_almost_equal(T_inv, b_T_a_exp)
 
-    """
-    def test_inverse_homogenous_transform2(self):
-        self.assertEqual(1, 0)
-    """
+    def test_camzTcamx(self):
+        '''
+        tests wether camz_T_camx returns homogeneous transformation that expresses camx_p in camz frame.
+        '''
+        camx_p = np.array([1,2,3,1])
+        camz_T_camx = camzTcamx()
+        camz_p = np.matmul(camz_T_camx, camx_p)
+
+        camz_p_exp = np.array([-2, -3, 1, 1])
+
+        np.testing.assert_almost_equal(camz_p, camz_p_exp)
+
+    def test_get_robot_pose(self):
+        q = [0.9585 0.0263 -0.2431 -0.1467]
+        t = [0.016 -0.041 0.295]
+
+        veh_R_tag, veh_t_tag = get_robot_pose(q,t)
+
+
 if __name__ == '__main__':
     rosunit.unitrun('apriltags2_ros', 'test_rotation_utils', RotationUtils)
