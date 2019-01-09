575889300a84594502f50199afa59b91b981f983
xiaohan <827330015@qq.com>
2019-01-09 04:06:54 -0800

    box plot for cpu and ram usage

---
+++ src/apriltags2_ros/apriltags2_ros/src/plot_cpu_ram.py
@@ -0,0 +1,80 @@
+import plotly.plotly as py
+from plotly import tools
+import plotly.graph_objs as go
+import os
+from ruamel import yaml
+import numpy as np
+import copy
+
+option = 3 # select from [1,2,3]
+option_str = {1:'cpu_load (%)', 2:'virt_mem (MB)', 3:'real_mem (MB)'}  # test option;  oz represents yaw
+
+path_name = os.getcwd()
+parent_path = os.path.dirname(path_name)
+summary_path = os.path.join(parent_path, "test_result", "summary")
+
+# template of the annotation to show the position of max 
+tem_annotation = {
+    "font":{"size":15},
+    "showarrow":False,
+    "text":None,    # str(max_position*100)+'%',
+    "x":None,
+    "y":None
+}
+annotation = []
+
+# a box plot for each x
+for x in range(10,11,2):
+    fig = tools.make_subplots(rows=1, cols=2, subplot_titles=('max usage','mean usage'))
+
+    for filename in os.listdir(summary_path):
+        groundtruth_x = float(filename.split('_')[2])
+        if groundtruth_x != x:
+            continue;
+        groundtruth_y = float(filename.split('_')[3])
+        groundtruth_yaw = float(filename.split('_')[4])  
+
+        with open(summary_path + '/' + filename, 'r') as f:
+            summary = yaml.load(f.read())   
+            data_mean = summary['cpu/ram']['cpu_ram_info'][option_str[option]]['mean']
+            data_max = summary['cpu/ram']['cpu_ram_info'][option_str[option]]['max']
+            total_num = summary['cpu/ram']['total number of cpu/ram messages']
+            max_position =  round(float(summary['cpu/ram'][option_str[option]]['index of max']) / total_num + 0.05, 1)                
+            
+            temp = copy.copy(tem_annotation) 
+            temp['text'] = str(max_position*100)+'%'
+            temp['x'] = groundtruth_y 
+            temp['y'] = max(data_max) + 5 
+            annotation.append(temp)
+
+            trace_mean = go.Box(
+                            name = str(groundtruth_y), 
+                            x = [groundtruth_y]*total_num,
+            		        y = data_mean
+            	        )
+            trace_max =go.Box(
+                            name = str(groundtruth_y), 
+                            x = [groundtruth_y]*total_num,
+            		        y = data_max,
+                            text = str(max_position*100)+'%'
+            	        )
+
+            fig.append_trace(trace_max,1,1)
+            fig.append_trace(trace_mean,1,2)
+
+    #add title and axis name
+    fig['layout']['xaxis1'].update(title='y(cm)', titlefont={'size':20})
+    fig['layout']['yaxis1'].update(title='max ' + option_str[option], titlefont={'size':20}, autorange = True)
+    fig['layout']['xaxis2'].update(title='y(cm)', titlefont={'size':20})
+    fig['layout']['yaxis2'].update(title='mean ' + option_str[option], titlefont={'size':20}, autorange = True)
+    fig['layout'].update(
+        title = option_str[option] + ' (x= ' + str(x) + 'cm, yaw= '+ str(groundtruth_yaw) + ')', 
+        titlefont={'size':30},
+    )
+    fig['layout']['annotations'] = annotation
+
+    
+    file_name = option_str[option] + " - x:" + str(10)
+    py.plot(fig, filename=option_str[option] + ' (x= ' + str(x) + 'cm, yaw= '+ str(groundtruth_yaw) + ')')
+
+
--- src/apriltags2_ros/apriltags2_ros/src/plot_mismatch.py
+++ src/apriltags2_ros/apriltags2_ros/src/plot_mismatch.py
@@ -3,9 +3,10 @@ import plotly.graph_objs as go
 import os
 from ruamel import yaml
 import numpy as np
+import math
 
 
-option = 3 # select from [1,2,3]
+option = 1 # select from [1,2,3]
 mismatch_option = {1:'x (cm)', 2:'y (cm)', 3:'oz (degree)'}  # test option;  oz represents yaw
 
 groundtruth = {'x (cm)':[], 'y (cm)':[], 'oz (degree)':[]} # oz represents yaw
@@ -18,6 +19,7 @@ parent_path = os.path.dirname(path_name)
 summary_path = os.path.join(parent_path, "test_result", "summary")
 
 for filename in os.listdir(summary_path):
+    # obtain groundtruth from filenames
     groundtruth['x (cm)'].append(float(filename.split('_')[2])) 
     groundtruth['y (cm)'].append(float(filename.split('_')[3]))
     groundtruth['oz (degree)'].append(float(filename.split('_')[4]))
@@ -27,21 +29,20 @@ for filename in os.listdir(summary_path):
         test_mean = summary['apriltag_output'][mismatch_option[option]]['mean'] 
         test_variance = summary['apriltag_output'][mismatch_option[option]]['variance']
         test_groundtruth = groundtruth[mismatch_option[option]][-1]
-        if option == 1: # I forget to add 10cm to the distance between camera and apriltag
+        # groundtruth[x] is the distance between camera and apriltag, we need to add 10 cm to get distance between duckiebot and apriltag
+        if option == 1: 
             test_groundtruth = np.array(test_groundtruth) + 10
         mismatch.append(abs(test_mean-test_groundtruth))
         variance.append(test_variance)
         text.append('mismatch: ' + str(abs(test_mean-test_groundtruth)) + '<br>variance: ' + str(test_variance))
 
 
-# size represents variance for yaw
-if option == 3:
-    size = np.array(variance)*50000   #for yaw, we include the variance information in the plot
-else:
-    size = [10] * len(variance)  # for x and y, we do not include varinance information in the plots
-
-# size = np.array(variance)
-# print(size)
+size = []
+if option == 3:   # size represents variance when plotting for mismatch of yaw
+    for i in range(len(variance)):
+        size.append(60 + math.log(variance[i], 10)*10)
+else:    # for mismatch for x and y, we do not include varinance information in the plots
+    size = [10] * len(variance)  
 
 data = [
     {
@@ -57,6 +58,28 @@ data = [
     }
 ]
 
-file_name = "mismatch - " + mismatch_option[option] + " - yaw:" + str(groundtruth['oz (degree)'][0])
-py.plot(data, filename=file_name)
+#add title and axis name
+layout = go.Layout(
+    title = 'mismatch for ' + mismatch_option[option] + ' (yaw= '+ str(groundtruth['oz (degree)'][0]) + ')',
+    titlefont = {
+            'size':30
+    },
+    xaxis = {
+        'title':'x(cm)',
+        'titlefont':{
+            'size':20
+        }
+    },
+    yaxis = {
+        'title':'y(cm)',
+        'titlefont':{
+            'size':20
+        }
+    }
+)
+
+fig = go.Figure(data=data, layout=layout)
+
+file_name = 'mismatch for ' + mismatch_option[option] + ' (yaw= '+ str(groundtruth['oz (degree)'][0]) + ')'
+py.plot(fig, filename=file_name)
 
